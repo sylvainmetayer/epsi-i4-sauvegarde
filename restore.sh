0277a724d9e9a362a136c7b7e0e23d24b543c4f3
Sylvain METAYER <sylvain_metayer@outlook.fr>
2018-02-26 17:36:24 +0100

    Restore files OK

--- restore.sh
+++ restore.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
+# shellcheck disable=2029
+# shellcheck disable=2069
 
 # make sure that SSH key are available, or script will wait for password.
 # Also make sure that user has write permissions on destination folder
 SSH_DETAILS="epsi-nextcloud@192.168.56.101"
 BACKUP_LOCATION="/home/user"
-BASENAME_BACKUP="archive" # .$(date).tar
+RESTAURATION_LOCATION="/home/user/test"
 
 # Do not edit below
-CURRENT_DATE=$(date +%F__%H_%M_%S)
+CURRENT_DATE=$(date +%s)
 LOG_FILE="/tmp/restore_${CURRENT_DATE}.log"
-my_true=true
-my_false=false
 
-# shellcheck disable=2069
+clear
 exec 2>&1 &> >(tee "${LOG_FILE}")
 echo "$LOG_FILE"
 
@@ -21,13 +21,17 @@ do
     echo "Which one to restore ? (ctrl-c to quit)"
     iterator=0
     for file in ${BACKUP_LOCATION}/*; do
-        if [[ -d $file ]]; then
-            # TODO Here indicate how many incremental + complete are present in folder (1 complete max, N incremntal)
-            iterator=$((iterator + 1))
-            echo "${iterator} - $file"
+        iterator=$((iterator + 1))
+        echo -n "${iterator} - "
+        if test -d "$file"; then
+            echo "Restore last incremental backup"
+        else 
+            filename=$(basename "$file")
+            format_date=$(date -d @"${filename%%.*}")
+            echo "$format_date"
         fi
     done
-    
+
     iterator_max=$iterator
     echo -n "Your choice : "
     read -r user_choice
@@ -46,28 +50,23 @@ done
 
 iterator=0
 for file in ${BACKUP_LOCATION}/*; do
-    if [[ -d $file ]]; then
-        iterator=$((iterator + 1))
-        if [[ $user_choice -eq $iterator ]]; then 
-            current_folder=$file
-        fi
-    fi
-done
-
-iterator=0
-for file in ${BACKUP_LOCATION}/$(basename "${current_folder}")/${BASENAME_BACKUP}.*.tar.gz; do
     iterator=$((iterator + 1))
-    if [[ $iterator -eq 1 ]]; then
-        echo "I will restore complete $file"
-    else 
-        echo "Now that I have restore a complete, I will restore a incremental one."
-        echo "$file"
+    if [[ $user_choice -eq $iterator ]]; then 
+        restauration_file=$file
     fi
 done
 
-# TODO Restore snippet
-#ssh $SSH_DETAILS rm -rf "$nextcloud_directory" && mkdir -p "$nextcloud_directory"
-# Upload tar
-#scp "${backup_directory}$archive" "${SSH_DETAILS}:${nextcloud_directory}"
-# Untar and remove archive file
-#ssh $SSH_DETAILS tar xf "${nextcloud_directory}/$archive" && rm "${nextcloud_directory}/$archive"
+ssh $SSH_DETAILS rm -rf "$RESTAURATION_LOCATION && mkdir -p $RESTAURATION_LOCATION"
+if test -f "$restauration_file"; then
+    filename=$(basename "$restauration_file")
+    echo "I will restore $restauration_file"
+    scp "$restauration_file" "${SSH_DETAILS}:${RESTAURATION_LOCATION}/"
+    ssh $SSH_DETAILS "tar xf ${RESTAURATION_LOCATION}/$filename -C ${RESTAURATION_LOCATION} && rm -f ${RESTAURATION_LOCATION}/$filename"
+else 
+    echo "I will restore last incremental backup : $file"
+    scp -r "${restauration_file}"/* ${SSH_DETAILS}:${RESTAURATION_LOCATION}
+fi
+
+echo "Restauration successful !"
+
+# TODO Database restauration
